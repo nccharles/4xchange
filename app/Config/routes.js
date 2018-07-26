e416d6742403361d9e6fe7379d1dcd3c9e8d25be
Luc.A <luc.bayo@gmail.com>
2018-07-26 13:59:50 +0200

    FlagAndCountryName

--- app/Config/routes.js
+++ app/Config/routes.js
@@ -1,74 +1,74 @@
-import React from 'react'
-import { TouchableOpacity, View } from 'react-native'
-import { StackNavigator, SwitchNavigator} from 'react-navigation'
-import { FontAwesome } from '@expo/vector-icons'
+import React from "react";
+import { TouchableOpacity, View } from "react-native";
+import { StackNavigator, SwitchNavigator } from "react-navigation";
+import { FontAwesome } from "@expo/vector-icons";
 
-import TabNavScreen from './tabNav'
-import WelcomeScreen from '../Screens/Welcome/Welcome'
+import TabNavScreen from "./tabNav";
+import WelcomeScreen from "../Screens/Welcome/Welcome";
 
-import Local from '../Screens/UserApp/Local'
-import CurrencyList from '../Screens/UserApp/CurrencyList'
-import Details from '../Screens/UserApp/Details'
-import MapView from '../Screens/UserApp/Map'
-import International from '../Screens/UserApp/International'
-import Login from '../Screens/BureauApp/Login'
-import Signup from '../Screens/BureauApp/Signup'
-import Info from '../Screens/BureauApp/Info'
-import Agreement from '../Screens/BureauApp/agreement'
-import AddCurrency from '../Screens/BureauApp/AddCurrency'
-import InfoReg from '../Screens/BureauApp/infoRegis'
-import AdditionalInfo from '../Screens/BureauApp/additionalInfo'
-import Country from '../Screens/BureauApp/countryList'
+import Local from "../Screens/UserApp/Local";
+import CurrencyList from "../Screens/UserApp/CurrencyList";
+import Details from "../Screens/UserApp/Details";
+import MapView from "../Screens/UserApp/Map";
+import International from "../Screens/UserApp/International";
+import Login from "../Screens/BureauApp/Login";
+import Signup from "../Screens/BureauApp/Signup";
+import Info from "../Screens/BureauApp/Info";
+import Agreement from "../Screens/BureauApp/agreement";
+import AddCurrency from "../Screens/BureauApp/AddCurrency";
+import InfoReg from "../Screens/BureauApp/infoRegis";
+import AdditionalInfo from "../Screens/BureauApp/additionalInfo";
+import Country from "../Screens/BureauApp/countryList";
 // import Intro from '../Screens/Intro'
-
 
 // Manifest of possible screens
 const InfoRegis = StackNavigator({
-  InfoReg:{
+  InfoReg: {
     screen: InfoReg,
     navigationOptions: {
       header: () => null
     }
   },
   Country: {
-    screen: Country,
+    screen: Country
   }
-})
+});
 
 const RegistrationNavigator = SwitchNavigator({
   Signup: { screen: Signup },
-  InfoRegis: {screen:InfoRegis},
-  AdditionalInfo: {screen: AdditionalInfo},
-})
+  InfoRegis: { screen: InfoRegis },
+  AdditionalInfo: { screen: AdditionalInfo },
+  Country: { screen: Country }
+});
 const WelcomeStack = StackNavigator({
   // InfoRegis: {screen:InfoRegis},
   WelcomeScreen: {
     screen: WelcomeScreen,
     navigationOptions: {
       header: () => null
-    } 
+    }
   },
-  Country:{screen: Country},
+  Country: { screen: Country },
   TabNavScreen: { screen: TabNavScreen },
   Local: { screen: Local },
   MapView: { screen: MapView },
   International: { screen: International },
   CurrencyList: { screen: CurrencyList },
-  Agreement: {screen: Agreement},
-  Details: { 
+  Agreement: { screen: Agreement },
+  Details: {
     screen: Details,
     navigationOptions: {
       header: () => null
     }
-  },
-})
+  }
+});
 const UserStack = StackNavigator({
   TabNavScreen: { screen: TabNavScreen },
   Local: { screen: Local },
   MapView: { screen: MapView },
   International: { screen: International },
   CurrencyList: { screen: CurrencyList },
-  Details: { 
+  Details: {
     screen: Details,
     navigationOptions: {
       header: () => null
@@ -78,21 +78,21 @@ const UserStack = StackNavigator({
     screen: WelcomeScreen,
     navigationOptions: {
       header: () => null
-    } 
-  },
-})
+    }
+  }
+});
 const SignedIn = StackNavigator({
-  AddCurrency: { 
+  AddCurrency: {
     screen: AddCurrency,
     navigationOptions: {
       header: () => null
     }
   },
-  CurrencyList: {screen: CurrencyList},
-  Info: { screen: Info },
-})
+  CurrencyList: { screen: CurrencyList },
+  Info: { screen: Info }
+});
 
-export default PrimaryNav = (initialRouter ="WelcomeStack") => {
+export default (PrimaryNav = (initialRouter = "WelcomeStack") => {
   return SwitchNavigator(
     {
       SignedIn: {
@@ -101,26 +101,26 @@ export default PrimaryNav = (initialRouter ="WelcomeStack") => {
       WelcomeStack: {
         screen: WelcomeStack
       },
-      UserStack:{
+      UserStack: {
         screen: UserStack
       },
       Signup: {
-         screen: Signup 
+        screen: Signup
       },
       InfoRegis: {
-        screen:InfoRegis
+        screen: InfoRegis
       },
       AdditionalInfo: {
         screen: AdditionalInfo
       },
-      Login: { 
-        screen: Login,
-      },
+      Login: {
+        screen: Login
+      }
     },
     {
       initialRouteName: `${initialRouter}`,
-      headerMode: 'float',
-      mode: 'modal',
+      headerMode: "float",
+      mode: "modal"
     }
   );
-};
+});
--- app/Screens/BureauApp/infoRegis.js
+++ app/Screens/BureauApp/infoRegis.js
@@ -1,252 +1,272 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import {
-    View,
-    KeyboardAvoidingView,
-    ScrollView,
-    TouchableOpacity,
-    Text,
-    Image,
-    Dimensions
-} from 'react-native'
-import {
-    Icon,
-    Button,
-    Input
-} from 'react-native-elements'
-import {
-  WaveIndicator,
-} from 'react-native-indicators';
-import AwesomeAlert from 'react-native-awesome-alerts'
-import SVGImage from 'react-native-svg-image'
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  Image,
+  Dimensions
+} from "react-native";
+import { Icon, Button, Input } from "react-native-elements";
+import { WaveIndicator } from "react-native-indicators";
+import AwesomeAlert from "react-native-awesome-alerts";
+import SVGImage from "react-native-svg-image";
 
-import styles from './Style/SignupStyles'
-import { Colors } from '../../Assets/Themes'
+import styles from "./Style/SignupStyles";
+import { Colors } from "../../Assets/Themes";
 
 //backend things
-import * as firebase from 'firebase'
+import * as firebase from "firebase";
 
-const screenwidth = Dimensions.get('window').width
-const screenheight = Dimensions.get('window').height
+const screenwidth = Dimensions.get("window").width;
+const screenheight = Dimensions.get("window").height;
 
 class InfoRegis extends Component {
-    constructor(props) {
-     super(props);
-     this.state = {
-       credentails: {
+  constructor(props) {
+    super(props);
+    this.state = {
+      credentails: {
         tinNumber: null,
         licenceNumber: null,
         phoneNumber: null,
-        companyName: null,
-       },
-       countryName: 'Rwanda',
-       countries: null,
-       flag: 'https://restcountries.eu/data/rwa.svg',
-       isSubmitting: false,
-       showAlert: false,
-       errs:{
-        tinNumber: null,
-    }
-    //    isLoading: false,
-     };
-    }
-    static navigationOptions = ({ navigation }) => {
-        return {
-            headerTransparent: true,
-            headerTintColor: '#fff',
-        }
-    };
-
-    //Alert message that come when the page is opening
-    hideAlert = () => {
-        this._queryExistingRegInfo()
-        this.setState({
-          showAlert: false
-        });
-    };
-
-    //luc's backend things
-
-    async componentDidMount() {
-        const currentUser = firebase.auth().currentUser
-        const {uid, email} = currentUser
-        this.setState({
-            showAlert: true,
-            userId: uid,
-            email
-        })
-    }
-    _queryExistingRegInfo = () => {
-        const {userId} = this.state
-        firebase.database().ref(`infos/${userId}/businessInfo`)
-        .once('value')
-        .then(sanpshot =>{
-            this.setState({
-                credentails:{
-                    ...this.state.credentails,
-                    ...sanpshot.val()
-                }
-            })
-        }).catch(error => {
-            console.log(error)
-        })
-    }
-    _getCurrentUserLocation = async () => {
-        const { status } = await Permissions.askAsync(Permissions.LOCATION);
-        if (status !== 'granted') {
-            this.setState({
-                errorMessage: 'Permission to access location was denied',
-            });
-            return alert('Enable to Access your location');
-        }
-
-        const location = await Location.getCurrentPositionAsync({});
-        //const address = await Location.geocodeAsync()
-        //console.log(address)
-        const { latitude, longitude } = location.coords
-        this.setState(state => ({
-            info: {
-                ...state.info,
-                latitude,
-                longitude,
-            }
-        }));
-        //console.log(latitude, longitude)
-    }
-
-    _handleInput = (key, value) =>{
-        this.setState(state =>({
-            credentails:{
-                ...state.credentails,
-                [key]: value,
-            }
-        }))
-    }
-
-    _handleSignUp = async () =>{
-        const {
-            credentails: {tinNumber, licenceNumber, phoneNumber, companyName}, 
-            userId, 
-            email, 
-            errs, 
-            isSubmitting,
-            countryName,
-            flag
-        } = this.state
-        
-        if (isSubmitting) {
-            return
-        }
-        const that = this
-        this.setState({
-            isSubmitting: true,
-        })
-        if(tinNumber.length < 9){
-            this.setState({
-                isSubmitting: false,
-                errs:{
-                    tinNumber: 'Invalid tin number'
-                }
-            })
-        }
-        firebase.database().ref(`infos/${userId}/businessInfo`).set({
-            tinNumber,
-            licenceNumber,
-            companyName,
-            phoneNumber,
-            completed: true,
-            isSubmitting: false,
-            countryName,
-            flag
-        }).then(resp => {
-            that.setState({
-                errors:{
-                    tinNumber: null,
-                },
-                isSubmitting: false,
-            })
-            firebase.auth().currentUser.updateProfile({
-                displayName: companyName,
-                phoneNumber,
-                countryName,
-                flag
-            }).then(resp => {
-                that.props.navigation.navigate('AdditionalInfo', {userId, phoneNumber, companyName, email, countryName, flag, countryName,flag})
-            }).catch(error => {
-                console.log(error)
-            })
-             
-        }).catch( error => {
-            console.log(error)
-        })
-    }
-    //backend end
-
-    setCountry = async (country) => {
-        // const {baseCurrency} = country
-        this.setState({
-            countries: country,
-            countryName: country.countryName,
-            flag: country.countryFlag
-        })
-        // console.log(this.state.countries.countryName)
-    }
-    getCountry = async(setCountry)=>{
-        this.setState({
-          ...this.state,
-        })
-        this.props.navigation.navigate('Country', {setCountry: this.setCountry})
+        companyName: null
+      },
+      countryName: "Rwanda",
+      countries: null,
+      flag: "https://restcountries.eu/data/rwa.svg",
+      isSubmitting: false,
+      showAlert: false,
+      errs: {
+        tinNumber: null
       }
-    
+      //    isLoading: false,
+    };
+  }
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTransparent: true,
+      headerTintColor: "#fff"
+    };
+  };
 
-    render() {
-        return (
-            <View
-                onResponderRelease={(event) => { Keyboard.dismiss(); }}
-                style={styles.container}>
-                <Text style={styles.logo}>4xChange</Text>
-                <KeyboardAvoidingView
-                    behavior="padding"
-                    style={{ flex: 1 }}
-                    keyboardVerticalOffset={30}
-                >
-                    <ScrollView>
-                        <TouchableOpacity 
-                            onPress={()=>this.getCountry(this.state.countries)}
-                            style={styles.button}>
-                                <SVGImage
-                                        style={styles.flag_icon}
-                                        source={{uri:this.state.flag}}
-                                    />
-                            <Text 
-                                style={styles.country_text}>
-                                {this.state.countryName}
-                            </Text>
-                        </TouchableOpacity>
-                        <Input
-                            placeholder='Company name'
-                            leftIcon={{ type: 'material-community', name: 'city', color: Colors.snow }}
-                            containerStyle={styles.input}
-                            underlineColorAndroid={'transparent'}
-                            inputStyle={styles.inputStyle}
-                            autoCorrect={false}
-                            returnKeyType={"next"}
-                            onChangeText={(input) => this._handleInput('companyName', input)}
-                            value={this.state.credentails.companyName}
-                        />
-                        <Input
-                            placeholder='TIN number'
-                            leftIcon={{ type: 'foundation', name: 'database', color: Colors.snow }}
-                            containerStyle={styles.input}
-                            underlineColorAndroid={'transparent'}
-                            inputStyle={styles.inputStyle}
-                            autoCapitalize='none'
-                            autoCorrect={false}
-                            returnKeyType={"next"}
-                            onChangeText={(input) => this._handleInput('tinNumber', input)}
-                            value={this.state.credentails.tinNumber}
-                        />
-                        {/* <Text style={styles.texterr}>{this.state.errs.tinNumber}</Text> */}
-                        {/* <Input
+  //Alert message that come when the page is opening
+  hideAlert = () => {
+    this._queryExistingRegInfo();
+    this.setState({
+      showAlert: false
+    });
+  };
+
+  //luc's backend things
+
+  async componentDidMount() {
+    const currentUser = firebase.auth().currentUser;
+    const { uid, email } = currentUser;
+    this.setState({
+      showAlert: true,
+      userId: uid,
+      email
+    });
+  }
+  _queryExistingRegInfo = () => {
+    const { userId } = this.state;
+    firebase
+      .database()
+      .ref(`infos/${userId}/businessInfo`)
+      .once("value")
+      .then(sanpshot => {
+        this.setState({
+          credentails: {
+            ...this.state.credentails,
+            ...sanpshot.val()
+          }
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  _getCurrentUserLocation = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+      return alert("Enable to Access your location");
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+    //const address = await Location.geocodeAsync()
+    //console.log(address)
+    const { latitude, longitude } = location.coords;
+    this.setState(state => ({
+      info: {
+        ...state.info,
+        latitude,
+        longitude
+      }
+    }));
+    //console.log(latitude, longitude)
+  };
+
+  _handleInput = (key, value) => {
+    this.setState(state => ({
+      credentails: {
+        ...state.credentails,
+        [key]: value
+      }
+    }));
+  };
+
+  _handleSignUp = async () => {
+    const {
+      credentails: { tinNumber, licenceNumber, phoneNumber, companyName },
+      userId,
+      email,
+      errs,
+      isSubmitting,
+      countryName,
+      flag
+    } = this.state;
+
+    if (isSubmitting) {
+      return;
+    }
+    const that = this;
+    this.setState({
+      isSubmitting: true
+    });
+    if (tinNumber.length < 9) {
+      this.setState({
+        isSubmitting: false,
+        errs: {
+          tinNumber: "Invalid tin number"
+        }
+      });
+    }
+    firebase
+      .database()
+      .ref(`infos/${userId}/businessInfo`)
+      .set({
+        tinNumber,
+        licenceNumber,
+        companyName,
+        phoneNumber,
+        completed: true,
+        isSubmitting: false,
+        countryName,
+        flag
+      })
+      .then(resp => {
+        that.setState({
+          errors: {
+            tinNumber: null
+          },
+          isSubmitting: false
+        });
+        firebase
+          .auth()
+          .currentUser.updateProfile({
+            displayName: companyName,
+            phoneNumber,
+            countryName,
+            flag
+          })
+          .then(resp => {
+            that.props.navigation.navigate("AdditionalInfo", {
+              userId,
+              phoneNumber,
+              companyName,
+              email,
+              countryName,
+              flag
+            });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  //backend end
+
+  setCountry = async country => {
+    // const {baseCurrency} = country
+    this.setState({
+      countries: country,
+      countryName: country.countryName,
+      flag: country.countryFlag
+    });
+    // console.log(this.state.countries.countryName)
+  };
+  getCountry = async setCountry => {
+    this.setState({
+      ...this.state
+    });
+    this.props.navigation.navigate("Country", { setCountry: this.setCountry });
+  };
+
+  render() {
+    return (
+      <View
+        onResponderRelease={event => {
+          Keyboard.dismiss();
+        }}
+        style={styles.container}
+      >
+        <Text style={styles.logo}>4xChange</Text>
+        <KeyboardAvoidingView
+          behavior="padding"
+          style={{ flex: 1 }}
+          keyboardVerticalOffset={30}
+        >
+          <ScrollView>
+            <TouchableOpacity
+              onPress={() => this.getCountry(this.state.countries)}
+              style={styles.button}
+            >
+              <SVGImage
+                style={styles.flag_icon}
+                source={{ uri: this.state.flag }}
+              />
+              <Text style={styles.country_text}>{this.state.countryName}</Text>
+            </TouchableOpacity>
+            <Input
+              placeholder="Company name"
+              leftIcon={{
+                type: "material-community",
+                name: "city",
+                color: Colors.snow
+              }}
+              containerStyle={styles.input}
+              underlineColorAndroid={"transparent"}
+              inputStyle={styles.inputStyle}
+              autoCorrect={false}
+              returnKeyType={"next"}
+              onChangeText={input => this._handleInput("companyName", input)}
+              value={this.state.credentails.companyName}
+            />
+            <Input
+              placeholder="TIN number"
+              leftIcon={{
+                type: "foundation",
+                name: "database",
+                color: Colors.snow
+              }}
+              containerStyle={styles.input}
+              underlineColorAndroid={"transparent"}
+              inputStyle={styles.inputStyle}
+              autoCapitalize="none"
+              autoCorrect={false}
+              returnKeyType={"next"}
+              onChangeText={input => this._handleInput("tinNumber", input)}
+              value={this.state.credentails.tinNumber}
+            />
+            {/* <Text style={styles.texterr}>{this.state.errs.tinNumber}</Text> */}
+            {/* <Input
                             placeholder='Licence number'
                             leftIcon={{ type: 'font-awesome', name: 'drivers-license-o', color: Colors.snow }}
                             containerStyle={styles.input}
@@ -258,48 +278,56 @@ class InfoRegis extends Component {
                             onChangeText={(input) => this._handleInput('licenceNumber', input)}
                             value={this.state.credentails.licenceNumber}
                         /> */}
-                        <Input
-                            placeholder='Phone'
-                            leftIcon={{ type: 'simple-line-icon', name: 'phone', color: Colors.snow }}
-                            containerStyle={styles.input}
-                            underlineColorAndroid={'transparent'}
-                            inputStyle={styles.inputStyle}
-                            returnKeyType={"next"}
-                            keyboardType="numeric"
-                            onChangeText={value => this._handleInput('phoneNumber', value)}
-                            value={this.state.credentails.phoneNumber}
-                            editable={true}
-                        />
-                        <Button
-                            onPress={this._handleSignUp.bind(this)}
-                            title='Add'
-                            icon={{ type: 'material-community', name: 'account-plus-outline', color: '#fff' }}
-                            buttonStyle={styles.button}
-                            loading={this.state.isSubmitting}
-                            disabled={this.state.isSubmitting}
-                            disabledStyle={styles.button}
-                            activityIndicatorStyle={{color: 'white'}}
-                        />
-                    </ScrollView>
-                </KeyboardAvoidingView>
-                <AwesomeAlert
-                    show={this.state.showAlert}
-                    showProgress={false}
-                    title="Location"
-                    message="4xChange is going to take your current location please make sure you are currently sitting in your forex bureau"
-                    closeOnTouchOutside={true}
-                    closeOnHardwareBackPress={false}
-                    showCancelButton={false}
-                    showConfirmButton={true}
-                    cancelText="No, cancel"
-                    confirmText="Ok got it"
-                    confirmButtonColor={Colors.dark}
-                    onConfirmPressed={() => {
-                        this.hideAlert();
-                    }}
-                    />
-            </View>
-        )
-    }
+            <Input
+              placeholder="Phone"
+              leftIcon={{
+                type: "simple-line-icon",
+                name: "phone",
+                color: Colors.snow
+              }}
+              containerStyle={styles.input}
+              underlineColorAndroid={"transparent"}
+              inputStyle={styles.inputStyle}
+              returnKeyType={"next"}
+              keyboardType="numeric"
+              onChangeText={value => this._handleInput("phoneNumber", value)}
+              value={this.state.credentails.phoneNumber}
+              editable={true}
+            />
+            <Button
+              onPress={this._handleSignUp.bind(this)}
+              title="Add"
+              icon={{
+                type: "material-community",
+                name: "account-plus-outline",
+                color: "#fff"
+              }}
+              buttonStyle={styles.button}
+              loading={this.state.isSubmitting}
+              disabled={this.state.isSubmitting}
+              disabledStyle={styles.button}
+              activityIndicatorStyle={{ color: "white" }}
+            />
+          </ScrollView>
+        </KeyboardAvoidingView>
+        <AwesomeAlert
+          show={this.state.showAlert}
+          showProgress={false}
+          title="Location"
+          message="4xChange is going to take your current location please make sure you are currently sitting in your forex bureau"
+          closeOnTouchOutside={true}
+          closeOnHardwareBackPress={false}
+          showCancelButton={false}
+          showConfirmButton={true}
+          cancelText="No, cancel"
+          confirmText="Ok got it"
+          confirmButtonColor={Colors.dark}
+          onConfirmPressed={() => {
+            this.hideAlert();
+          }}
+        />
+      </View>
+    );
+  }
 }
 export default InfoRegis;
